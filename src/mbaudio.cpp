63774b7d0d5d7383c7562bde310ad1be36655bfc
John David Taylor <31504941+JDTAY87@users.noreply.github.com>
2017-12-14 04:24:01 -0500

    Simplified test sound constructor
    Pretty sure it plays an actual C4 when you type 'a' now.

--- src/mbaudio.cpp
+++ src/mbaudio.cpp
@@ -2,16 +2,11 @@
 
 mbAudio::mbAudio()
 {
-    int halfsteps = 9;
+    int halfsteps = (-9);
     double wavelength = 22050 / ( 440 * pow( 1.05946, halfsteps ) );
-    double phase = 0;
-    for ( int z = 0; z < 4000; z += 200 )
+    for ( int z = 0; z < 4000; z++ )
     {
-        for ( int z2 = 0; z2 < 200; z2++ )
-        {
-            audiodata[z+z2] = (Sint8)( 127*sin( (2*3.14159*(z2+phase))/wavelength ) );
-        }
-        phase = fmod( phase+200, wavelength );
+        audiodata[z] = (Sint8)( 127*sin( (2*3.14159*z)/wavelength ) );
     }
 }
 
